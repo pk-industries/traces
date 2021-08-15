b99094c24c7f12c5ab9ea8c532b1e5da3a80a375
pduren <57735971+kParkz@users.noreply.github.com>
2021-08-15 10:45:00 -0500

    bathroom logic init

--- bathroom.lua
+++ bathroom.lua
@@ -4,3 +4,34 @@ Gfx = love.graphics
 Bathroom = {}
 --by default, you have not "entered" the room. This must be set to true by walking through one of the doors to the room.
 Bathroom.entered = false
+
+function Bathroom:load()
+    BathWall1, BathWall2, SinkWall = Gfx.newImage("pics/BathWall1.png"), Gfx.newImage("pics/BathWall2.png"), Gfx.newImage("pics/SinkWall")
+    BathWalls = {BathWall1, BathWall2, SinkWall}
+    CurrentWall = BathWall1
+end
+
+function love.keypressed(key)
+    if CurrentWall == BathWall1 then
+        if key == "up" then
+            CurrentWall = SinkWall
+        elseif key == "down" then
+            --TODO: ENTER BACK INTO BEDROOM
+        end
+    elseif CurrentWall == SinkWall then
+        if key == "left" then
+            CurrentWall = BathWall2
+        elseif key == "down" then
+            CurrentWall = BathWall1
+        end
+    elseif CurrentWall == BathWall2 then
+        if key == "right" then
+            CurrentWall = SinkWall
+        --TODO: DO SHOWER WALL STUFF
+        end
+    end
+end
+
+function love.draw()
+    Gfx.draw(CurrentWall, 0, 0)
+end
