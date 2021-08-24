da80149378f02ab9c3657e106f50cbf1b42d6359
pduren <57735971+kParkz@users.noreply.github.com>
2021-08-24 16:18:55 -0500

    bedroom 4 walls movement init

--- bedroom.lua
+++ bedroom.lua
@@ -3,4 +3,44 @@
 Gfx = love.graphics
 Bedroom = {}
 --by default, you have not "entered" the room. set to true by walking thru room door (if it is declared as true here it is only for debugging, or it is the starting room)
-Bedroom.entered = false
+Bedroom.entered = true
+
+function Bedroom:load()
+
+    HallWall, BathWall, DresserWall, BedWall = Gfx.newImage("pics/HallWall.png"), Gfx.newImage("pics/BathWall.png"), Gfx.newImage("pics/DresserWall.png"), Gfx.newImage("pics/BedWall.png")
+    BedroomWalls = {HallWall, BathWall, DresserWall, BedWall}
+    CurrentWall = HallWall
+
+    function love.keypressed(key)
+        if CurrentWall == HallWall then
+           if key == "left" then
+            CurrentWall = DresserWall
+           elseif key == "right" then
+            CurrentWall = BathWall
+           end
+        elseif CurrentWall == DresserWall then
+           if key == "left" then
+            CurrentWall = BedWall
+           elseif key == "right" then
+            CurrentWall = HallWall
+           end
+        elseif CurrentWall == BedWall then
+            if key == "left" then
+             CurrentWall = BathWall
+            elseif key == "right" then
+             CurrentWall = DresserWall
+            end
+        elseif CurrentWall == BathWall then
+            if key == "left" then
+             CurrentWall = HallWall
+            elseif key == "right" then
+             CurrentWall = BedWall
+            end
+        end
+    end
+
+end
+
+function Bedroom:draw()
+    Gfx.draw(CurrentWall, 0, 0)
+end
