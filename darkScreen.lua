14c9943d3ed8dde3ba61459a198d2aca022bb7fb
pduren <57735971+kParkz@users.noreply.github.com>
2021-08-25 23:03:51 -0500

    reformat darkScreen even tho it still no work

--- darkScreen.lua
+++ darkScreen.lua
@@ -2,51 +2,31 @@
 --TODO: Use this on room change, most likely when [room].entered value changes.
 
 DarkScreen = {}
-DarkScreen.state = 0 -- 0 is stable, 1 is getting dark, -1 is getting lighter
-DarkScreen.alpha = 0
-DarkScreen.time = 1 -- time in seconds for the blackScreen fade/unfade
 
-GameWidth = 400
-GameHeight = 240
-Scale = 1
+function DarkScreen:load()
+
+  GameWidth = 400
+  GameHeight = 240
+  Scale = 1
+  DarkScreen.r = 255
+  DarkScreen.g = 255
+  DarkScreen.b = 255
+  
+end
+
+
 
 function DarkScreen:update(dt)
 
-  if self.state ~= 0 then
-    self.alpha = self.alpha + (self.state / self.time * dt)
-  end
-
-  if self.alpha < 0 then
-    self.alpha = 0
-    self.state = 0
-  end
-
-  if self.alpha > 1 then
-    self.alpha = 1
-    self.state = 0
-  end
+  DarkScreen.r = DarkScreen.r - 1
+  DarkScreen.g = DarkScreen.g - 1
+  DarkScreen.b = DarkScreen.b - 1 
 
 end
 
 function DarkScreen:draw()
 
-  love.graphics.setColor(1, 0, 0, self.alpha)
+  love.graphics.setColor(DarkScreen.r/255, DarkScreen.g/255, DarkScreen.b/255)
   love.graphics.rectangle("fill", -20, -20, (GameWidth + 40) * Scale, (GameHeight + 40) * Scale)
-
-end
-
-function DarkScreen:fadeIn(t)
-
-  self.alpha = 1
-  self.state = -1
-  self.time = t or 1
-
-end
-
-function DarkScreen:fadeOut(t)
-
-  self.alpha = 0
-  self.state = 1
-  self.time = t or 1
 
 end
