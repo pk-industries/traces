2c0bd9753ba942ca3913a3ae2c7f2770371efab3
Garrett Weems <gwgraphicdesign@gmail.com>
2021-10-03 01:02:06 -0500

    :building_construction: move states to own files

---
+++ states/game.lua
@@ -0,0 +1,27 @@
+local game = {}
+
+function game:init()
+
+end
+
+function game:enter()
+
+end
+
+function game:update(dt)
+
+end
+
+function game:keypressed(key, code)
+
+end
+
+function game:mousepressed(x, y, mbutton)
+
+end
+
+function game:draw()
+
+end
+
+return game
