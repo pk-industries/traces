f28261e05a01dfe7a571c2afa54800475dc39d00
Garrett Weems <gwgraphicdesign@gmail.com>
2021-10-19 23:56:37 -0500

    :bento: update bedroom images
    still using images from prototype folder as room images are completely

--- states/game.lua
+++ states/game.lua
@@ -31,21 +31,11 @@ function game.frame(self)
     --The center x point
     local posy = love.graphics.getHeight() / 2
     --The center y point
-    local ox = 0
-    local oy = 1
     local zoom = 1
     local sx = posx - (imgW * zoom / 2)
     local sy = posy - (imgH * zoom / 2)
     return img, sx, sy, 0, zoom, zoom
 end
-
--- local img = img --Your image
--- local imgW = img:getWidth() --Get your image size
--- local imgH = img:getHeight() --Get your image size
--- local zoom = n --Your zoom factor (to zoom in, must be bigger than 1)
-
---if you want it based on position:
--- love.graphics.draw(img, posx - (imgW * zoom / 2), posy - (imgH * zoom / 2), 0, zoom, zoom)
 
 function game:init()
     local save = table.load("save.lua")
@@ -59,7 +49,7 @@ end
 --- /path/to/your/img.png
 ---@return string
 function game.imgPath(self)
-    local imgDir = "assets/images/prototype"
+    local imgDir = "assets/images"
     local path =
         imgDir ..
         "/" .. self.room .. "/" .. "x" .. self.x .. "y" .. self.y .. "_" .. self.direction .. "_" .. self.room .. ".png"
@@ -80,72 +70,25 @@ function game.facingExit(self)
     return facing
 end
 
-function game.debugTxt(self)
-    local stats = {
-        "room: " .. self.room,
-        "direction: " .. self.direction,
-        "x: " .. self.x,
-        "y: " .. self.y
-    }
-    if game:facingExit() then
-        table.insert(stats, "facing exit: " .. game:facingExit())
-    end
-    local msg = ""
-    for _, val in ipairs(stats) do
-        msg = msg .. val .. "\n"
-    end
-    return msg
-end
-
 function game:enter()
 end
 
 function game:leave()
-    local errMsg =
-        table.save({["room"] = game.room, ["direction"] = game.direction, ["x"] = game.x, ["y"] = game.y}, "save.lua")
-
-    if errMsg then
-        print("file not created: " .. errMsg)
-    end
 end
 
 function game:update(dt)
-    local facing = game:facingExit()
-    if facing then
-        game.prompt = "Press " .. facing .. " to enter " .. facing .. " room"
-    -- print(game.prompt)
-    end
 end
 
 function game:keypressed(key, code)
-    local stats = {
-        "room: " .. self.room,
-        "direction: " .. self.direction,
-        "x: " .. self.x,
-        "y: " .. self.y
-    }
-    if game:facingExit() then
-        table.insert(stats, "facing exit: " .. game:facingExit())
-    end
-    local msg = ""
-    for _, val in ipairs(stats) do
-        -- print(val)
-    end
-
     game.zoom = 1
 end
 
 function game:keyreleased(key, code)
-    -- game.kx = 1
-    -- game.ky = 1
     if key == Controls.pause then
         GameState.switch(States.pause)
     else
         game:move(key)
     end
-end
-
-function game:mousepressed(x, y, mbutton)
 end
 
 function game:draw()
@@ -155,8 +98,10 @@ function game:draw()
     local imgW = img:getWidth()
     local imgH = img:getHeight()
     local zoom = game.zoom
-    love.graphics.draw(img, 0, 0)
-    -- love.graphics.draw(img, game.sx - (imgW * zoom / 2), game.sy - (imgH * zoom / 2), 0, zoom, zoom)
+    love.graphics.draw(img, game.sx - (imgW * zoom / 2), game.sy - (imgH * zoom / 2), 0, zoom, zoom)
+end
+
+function game:mousepressed(x, y, mbutton)
 end
 
 return game
