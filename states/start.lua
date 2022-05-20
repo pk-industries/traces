84c2124d308b177ef5b1a9d0baae14470443bb12
Jacob Camacho <jcamacho@wrightgroup.com>
2022-05-20 16:17:12 -0500

    Added start sound

--- states/start.lua
+++ states/start.lua
@@ -1,14 +1,19 @@
 local image, animation
+local btnSnd
 
 local StartSceen = {}
 
 function StartSceen:init()
+    -- Load Graphics
     image = System.graphics.createImage("assets/images/startscreen.png")
     local height = System.graphics.getImageHeight(image)
     local width = System.graphics.getImageWidth(image)
     local grid = Anim8.newGrid(width / 3, height, width, height)
     animation = Anim8.newAnimation(grid("1-3", 1), 0.15)
     System.graphics.setBackgroundColor(Colors.white)
+
+    -- Load Audio
+    btnSnd = System.audio.createSource("assets/sounds/button_press.wav", "static")
 end
 
 function StartSceen:enter()
@@ -41,8 +46,9 @@ function StartSceen:draw()
     System.graphics.setColor(1, 1, 1) -- white
 end
 
-function StartSceen:keypressed(key, scancode, isrepeat)
+function StartSceen:keypressed(key)
     if key == Controls.a then
+        System.audio.play(btnSnd)
         GameState.switch(States.game)
     elseif key == Controls.b then
         System.event.quit()
