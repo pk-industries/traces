4bcad5c0c30eb90cf47ce9e41ca156bc1d95199c
Jacob Camacho <jcamacho@wrightgroup.com>
2022-05-27 14:26:16 -0500

    Changed prompt to have a default speed of 0.01

--- house/bedroom/dresser.lua
+++ house/bedroom/dresser.lua
@@ -1,19 +1,27 @@
 local SimpleScene = require("house.scenetemplates").SimpleScene
 local MusicBox = require "house.bedroom.musicbox"
 local Dresser = Class {__includes = SimpleScene}
+local prompt
 
 function Dresser:init()
     SimpleScene.init(self, "bedroom.dresser", false, "assets/images/bedroom/dresser.png")
+    prompt = require("house.flashingprompt")("A", 292, 124)
 end
 
-function Dresser:update()
-    if System.keyboard.isDown(Controls.up) and System.keyboard.isDown(Controls.left) then
-        GameState.push(MusicBox)
-    end
+function Dresser:enter()
+    prompt.on = true
+    SimpleScene.enter(self)
+end
+
+function Dresser:draw()
+    SimpleScene.draw(self)
+    prompt:routine()
 end
 
 function Dresser:keypressed(key)
-    if key == Controls.down then
+    if key == Controls.up then
+        GameState.push(MusicBox)
+    elseif key == Controls.down then
         GameState.pop()
     end
     SimpleScene.keypressed(self, key)
